6e41b28f7c4d32ab38448f120020ec52d1493843
gabrielxgm <gabrielmotiva@gmail.com>
2024-08-22 20:39:40 -0300

    Criando o main

---
+++ Main.hs
@@ -0,0 +1,30 @@
+
+
+main :: IO()
+main = do
+    putStrLn "Bem vindo a caixa registradora P.L.P.!\nPor favor escolha uma das opções abaixo:"
+    putStrLn "1.Criar login de acesso\n2.Efetuar login"
+    putStrLn "3.Criar produto\n4.Ler produto\n5.Atualizar produto\n6.Deletar produto"
+    putStrLn "7.Gerar relatório"
+    putStrLn "Sair"
+
+    input <- getLine
+
+    when(input=="Sair") $ do
+        putStrLn "Fechando programa..."
+        return()
+    
+    let escolha = read input :: Int
+    
+    case escolha of
+        1 -> 
+        2 ->
+        3 ->
+        4 ->
+        5 ->
+        6 ->
+        7 ->
+        _ -> putStrLn "Opção inválida"
+
+    if escolha `notElem` [1,2,3,4,5,6,7]
+        then main
