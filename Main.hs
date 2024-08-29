8ce97a8363b7eb0bac8d977ce7c7fa21b561f088
gabrielxgm <gabrielmotiva@gmail.com>
2024-08-29 16:51:40 -0300

    ajeitando o menu

--- Main.hs
+++ Main.hs
@@ -1,11 +1,8 @@
 import GeradorDeCupons
 
-main :: IO()
-main = do
-
-    hashCodigoCupom <- criarTabelaHashVazia
-
-    putStrLn "Bem vindo a caixa registradora P.L.P.!\nPor favor escolha uma das opções abaixo:"
+exibirMenu :: IO()
+exibirMenu = do
+    putStrLn "Por favor escolha uma das opções abaixo:"
     putStrLn "1.Criar login de acesso\n2.Efetuar login"
     putStrLn "3.Criar produto\n4.Ler produto\n5.Atualizar produto\n6.Deletar produto"
     putStrLn "7.Gerar relatório"
@@ -16,18 +13,28 @@ main = do
     when(input=="Sair") $ do
         putStrLn "Fechando programa..."
         return()
-    
+
     let escolha = read input :: Int
-    
+
     case escolha of
-        1 -> 
-        2 ->
-        3 ->
-        4 ->
-        5 ->
-        6 ->
-        7 ->
+        "1" -> 
+        "2" ->
+        "3" ->
+        "4" ->
+        "5" ->
+        "6" ->
+        "7" ->
         _ -> putStrLn "Opção inválida"
 
     if escolha `notElem` [1,2,3,4,5,6,7]
-        then main
+        then exibirMenu
+
+main :: IO()
+main = do
+
+    hashCodigoCupom <- criarTabelaHashVazia
+
+    putStrLn "Bem vindo a caixa registradora P.L.P.!"
+
+    exibirMenu
+  
