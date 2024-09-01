28cc78b9947f454c039e9971e5a2fe76357e4d3c
gabrielxgm <127308223+gabrielxgm@users.noreply.github.com>
2024-09-01 11:33:44 -0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -6,7 +6,7 @@ import Login (bancoDeDados)
 import Data.IORef (newIORef)
 
 
--- ghc -package containers -o programa Main.hs para compilar o programa
+-- ghc -package containers -package directory -o programa Main.hs para compilar o programa
 -- .\programa.exe para rodar o programa
 
 main :: IO ()
