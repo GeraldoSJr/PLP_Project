82e8cf882898b1e96623eff2da3d748515e81f5a
gabrielxgm <gabriel.xavier.gomes.maia@ccc.ufcg.edu.br>
2024-10-04 17:38:16 -0300

    Finalizando menu e gerador de cupons

--- Prolog_Project/geradorDeCupons.pl
+++ Prolog_Project/geradorDeCupons.pl
@@ -1,4 +1,4 @@
-:- module(geradorDeCupons, [gerar_cupom/3, armazenar_tabela/1]).
+:- module(geradorDeCupons, [gerar_cupom/3, verificar_cupom/2]).
 
 :- use_module(library(assoc)).
 
@@ -6,18 +6,17 @@ gerar_cupom(TabelaHashAtual, Desconto, TabelaFinal) :-
     gerar_codigo_cupom(Cupom),
     (   get_assoc(Cupom, TabelaHashAtual, _)
     ->  gerar_cupom(TabelaHashAtual, Desconto, TabelaFinal)
-    ;   put_assoc(Cupom, TabelaHashAtual, desconto(Desconto), TabelaFinal),
-        write('Novo cupom gerado: '), write(Cupom), nl
+    ;   put_assoc(Cupom, TabelaHashAtual, Desconto, TabelaFinal),
+        write('Novo cupom gerado: '), write(Cupom), 
+        write(', Desconto: '), write(Desconto),write('%'), nl
     ).
 
 gerar_codigo_cupom(Cupom) :-
     random(1000, 9999, Numero),
-    atom_concat('CUP-', Numero, Cupom).
+    atom_number(Cupom, Numero). 
 
-armazenar_tabela(TabelaHash) :-
-    write('Tabela final de cupons atualizada:'), nl,
-    forall(
-        (   assoc_to_list(TabelaHash, List),
-            member(Key-Value, List)),
-        (   write(Key), write(': '), write(Value), nl)
+verificar_cupom(Cupom, TabelaHash) :-
+    (   get_assoc(Cupom, TabelaHash, Desconto)
+    ->  format('Cupom ~w encontrado com desconto: ~w%~n', [Cupom, Desconto])
+    ;   write('Cupom nao encontrado.'), nl
     ).
--- Prolog_Project/menu.pl
+++ Prolog_Project/menu.pl
@@ -11,47 +11,48 @@ menu(TabelaHashCupom) :-
     write('7. '), nl,
     write('8. Criar cupom desconto '), nl,
     write('9. Verificar cupom de desconto '), nl,
-    write('Sair.'), nl,
+    write('10. Aplicar desconto no produto'), nl,
     write('Escolha uma opcao: '),
     read(Opcao),
     executar(Opcao, TabelaHashCupom).
 
-executar(1, TabelaHashCupom) :-
-    menu(TabelaHashCupom).  
+executar(1, TabelaHashCupom) :- 
+    menu(TabelaHashCupom).
 
-executar(2, TabelaHashCupom) :-
-    menu(TabelaHashCupom).  
+executar(2, TabelaHashCupom) :- 
+    menu(TabelaHashCupom).
 
-executar(3, TabelaHashCupom) :-
-    menu(TabelaHashCupom).  
+executar(3, TabelaHashCupom) :-     
+    menu(TabelaHashCupom).
 
-executar(4, TabelaHashCupom) :-
-    menu(TabelaHashCupom).  
+executar(4, TabelaHashCupom) :- 
+    menu(TabelaHashCupom).
+    
+executar(5, TabelaHashCupom) :- 
+    menu(TabelaHashCupom).
 
-executar(5, TabelaHashCupom) :-
-    menu(TabelaHashCupom).  
+executar(6, TabelaHashCupom) :- 
+    menu(TabelaHashCupom).
 
-executar(6, TabelaHashCupom) :-
-    menu(TabelaHashCupom). 
-
-executar(7, TabelaHashCupom) :-
-    menu(TabelaHashCupom).  
+executar(7, TabelaHashCupom) :- 
+    menu(TabelaHashCupom).
 
 executar(8, TabelaHashCupom) :-
     write('Digite a porcentagem de desconto: '),
     read(Desconto),
-    gerar_cupom(TabelaHashCupom, Desconto, NovaTabelaHash), 
-    armazenar_tabela(NovaTabelaHash),  
-    menu(NovaTabelaHash). 
+    gerar_cupom(TabelaHashCupom, Desconto, NovaTabelaHash),  
+    menu(NovaTabelaHash).
 
 executar(9, TabelaHashCupom) :-
-    menu(TabelaHashCupom). 
-     
-executar("Sair.",_) :-
-    write('Saindo do programa...'), nl,
+    write('Digite o codigo do cupom: '),
+    read(CupomInput),
+    atom_number(CupomAtom, CupomInput),
+    verificar_cupom(CupomAtom, TabelaHashCupom),
+    menu(TabelaHashCupom).
+
+executar(10, TabelaHashCupom) :-
     menu(TabelaHashCupom).
 
 executar(_, TabelaHashCupom) :-
     write('Opcao invalida! Tente novamente.'), nl,
-    menu(TabelaHashCupom).  
-
+    menu(TabelaHashCupom).
