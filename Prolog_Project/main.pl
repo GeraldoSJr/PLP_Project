3efeb7c1825d4c6ec02bc01912f5ae5e63b2ebf0
gabrielxgm <gabriel.xavier.gomes.maia@ccc.ufcg.edu.br>
2024-10-04 17:40:43 -0300

    finalizando o main e o start

--- Prolog_Project/main.pl
+++ Prolog_Project/main.pl
@@ -1,5 +1,8 @@
 :- use_module(geradorDeCupons).
 
+:- use_module(menu).
+
+/* use [start]. para iniciar o programa.*/
 main :-
     write('Bem-vindo a caixa registradora P.L.P.'), nl,
     empty_assoc(TabelaHashCupom),
--- Prolog_Project/start.pl
+++ Prolog_Project/start.pl
@@ -1,3 +1,5 @@
+/* coloquem os arquivos na ordem certa, os menos importantes/dependentes ficam no topo */
+
 :- [geradorDeCupons].
 :- [menu].
 :- [main].
