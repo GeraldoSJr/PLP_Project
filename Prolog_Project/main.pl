a270c5517b3d315c3cecd68a5d1c53469b74cdcc
GeraldoSJr <gsmecanica76@gmail.com>
2024-10-06 19:59:29 -0300

    report

--- Prolog_Project/main.pl
+++ Prolog_Project/main.pl
@@ -1,12 +1,15 @@
 :- use_module(geradorDeCupons).
 :- use_module(menu).
 :- use_module(login).
+:- use_module(relatorio).  % Include the relatorio module
 
 /* use [main]. to start the program. */
 main :-
     write('Bem-vindo a caixa registradora P.L.P.'), nl,
     empty_assoc(TabelaHashCupom),
     Funcionarios = [],
+    limpar_relatorio,  % Clear the report on startup
+    registrar_acao('Sistema iniciado'),  % Log system startup
     menu(TabelaHashCupom, Funcionarios, none).
 
 :- initialization(main).
--- Prolog_Project/menu.pl
+++ Prolog_Project/menu.pl
@@ -3,6 +3,7 @@
 :- use_module(login).
 :- use_module(item).
 :- use_module(estoque).
+:- use_module(relatorio).  % Include the relatorio module
 
 menu(TabelaHashCupom, Funcionarios, FuncaoAtual) :- 
     write('--- Menu Principal ---'), nl,
@@ -16,7 +17,9 @@ menu(TabelaHashCupom, Funcionarios, FuncaoAtual) :-
     write('8. Criar cupom desconto'), nl,
     write('9. Verificar cupom de desconto'), nl,
     write('10. Aplicar desconto no produto'), nl,
-    write('11. Sair'), nl,
+    write('11. Gerar relatório'), nl,
+    write('12. Limpar relatório'), nl,
+    write('13. Sair'), nl,
     write('Escolha uma opcao: '),
     read(Opcao),
     executar(Opcao, TabelaHashCupom, Funcionarios, FuncaoAtual).
@@ -27,6 +30,7 @@ executar(1, TabelaHashCupom, Funcionarios, FuncaoAtual) :-
     write('Digite a função (gerente/caixa): '), read(Funcao),
     (criar_login(NomeUsuario, Senha, Funcao, Funcionarios, NovosFuncionarios) ->
         write('Login criado com sucesso!'), nl,
+        registrar_acao('Login criado com sucesso'),  % Log action
         menu(TabelaHashCupom, NovosFuncionarios, FuncaoAtual)
     ;
         write('Falha ao criar login. Verifique os dados.'), nl,
@@ -39,9 +43,11 @@ executar(2, TabelaHashCupom, Funcionarios, _) :-
     efetuar_login(NomeUsuario, Senha, Funcionarios, NovaFuncao),
     (NovaFuncao \= none ->
         write('Login efetuado com sucesso como: '), write(NovaFuncao), nl,
+        registrar_acao('Login efetuado com sucesso'),  % Log action
         menu(TabelaHashCupom, Funcionarios, NovaFuncao)
     ; 
         write('Falha no login. Verifique suas credenciais.'), nl,
+        registrar_acao('Falha no login'),  % Log action
         menu(TabelaHashCupom, Funcionarios, none)
     ).
 
@@ -50,20 +56,24 @@ executar(3, TabelaHashCupom, Funcionarios, FuncaoAtual) :-
     write('Digite a quantidade em estoque: '), read(Estoque),
     write('Digite o preço: '), read(Preco),
     (adicionar_item(Nome, Estoque, Preco) ->
-        write('Item adicionado com sucesso.'), nl
+        write('Item adicionado com sucesso.'), nl,
+        registrar_acao('Item adicionado'),  % Log action
+        menu(TabelaHashCupom, Funcionarios, FuncaoAtual)
     ;
-        write('Falha ao adicionar item.'), nl
-    ),
-    menu(TabelaHashCupom, Funcionarios, FuncaoAtual).
+        write('Falha ao adicionar item.'), nl,
+        menu(TabelaHashCupom, Funcionarios, FuncaoAtual)
+    ).
 
 executar(4, TabelaHashCupom, Funcionarios, FuncaoAtual) :- 
     write('Digite o ID do item: '), read(Id),
     (ler_item(Id, Item) ->
-        write('Item encontrado: '), write(Item), nl
+        write('Item encontrado: '), write(Item), nl,
+        registrar_acao('Item lido'),  % Log action
+        menu(TabelaHashCupom, Funcionarios, FuncaoAtual)
     ;
-        write('Item não encontrado.'), nl
-    ),
-    menu(TabelaHashCupom, Funcionarios, FuncaoAtual).
+        write('Item não encontrado.'), nl,
+        menu(TabelaHashCupom, Funcionarios, FuncaoAtual)
+    ).
 
 executar(5, TabelaHashCupom, Funcionarios, FuncaoAtual) :- 
     write('Digite o ID do item a ser atualizado: '), read(Id),
@@ -71,31 +81,37 @@ executar(5, TabelaHashCupom, Funcionarios, FuncaoAtual) :-
     write('Digite a nova quantidade em estoque: '), read(Estoque),
     write('Digite o novo preço: '), read(Preco),
     (atualizar_item(Id, item(Nome, Estoque, Preco)) ->
-        write('Item atualizado com sucesso.'), nl
+        write('Item atualizado com sucesso.'), nl,
+        registrar_acao('Item atualizado'),  % Log action
+        menu(TabelaHashCupom, Funcionarios, FuncaoAtual)
     ;
-        write('Falha ao atualizar item.'), nl
-    ),
-    menu(TabelaHashCupom, Funcionarios, FuncaoAtual).
+        write('Falha ao atualizar item.'), nl,
+        menu(TabelaHashCupom, Funcionarios, FuncaoAtual)
+    ).
 
 executar(6, TabelaHashCupom, Funcionarios, FuncaoAtual) :- 
     write('Digite o ID do item a ser deletado: '), read(Id),
     (deletar_item(Id) ->
-        write('Item deletado com sucesso.'), nl
+        write('Item deletado com sucesso.'), nl,
+        registrar_acao('Item deletado'),  % Log action
+        menu(TabelaHashCupom, Funcionarios, FuncaoAtual)
     ;
-        write('Falha ao deletar item.'), nl
-    ),
-    menu(TabelaHashCupom, Funcionarios, FuncaoAtual).
+        write('Falha ao deletar item.'), nl,
+        menu(TabelaHashCupom, Funcionarios, FuncaoAtual)
+    ).
 
 executar(7, TabelaHashCupom, Funcionarios, FuncaoAtual) :- 
     listar_itens(Itens),
     write('Itens disponíveis:'), nl,
     forall(member(Item, Itens), write(Item), nl),
+    registrar_acao('Itens listados'),  % Log action
     menu(TabelaHashCupom, Funcionarios, FuncaoAtual).
 
 executar(8, TabelaHashCupom, Funcionarios, FuncaoAtual) :- 
     (FuncaoAtual = gerente ->
         write('Digite a porcentagem de desconto: '), read(Desconto),
         gerar_cupom(TabelaHashCupom, Desconto, NovaTabelaHash),  
+        registrar_acao('Cupom gerado'),  % Log action
         menu(NovaTabelaHash, Funcionarios, FuncaoAtual)
     ; 
         write('Acesso negado. Somente gerentes podem criar cupons.'), nl,
@@ -106,30 +122,43 @@ executar(9, TabelaHashCupom, Funcionarios, FuncaoAtual) :-
     write('Digite o código do cupom: '), read(CupomInput),
     atom_number(CupomAtom, CupomInput),
     verificar_cupom(CupomAtom, TabelaHashCupom),
+    registrar_acao('Cupom verificado'),  % Log action
     menu(TabelaHashCupom, Funcionarios, FuncaoAtual).
 
 executar(10, TabelaHashCupom, Funcionarios, FuncaoAtual) :- 
     write('Digite o código do cupom: '), 
     read(CupomInput),
-    ( atom_number(CupomAtom, CupomInput) -> 
-        (remover_cupom(CupomAtom, TabelaHashCupom, NovaTabelaHash, Desconto) ->
-            write('Desconto aplicado: '), write(Desconto), write('%'), nl,
-            atualizar_precos_com_desconto(Desconto),  % Atualiza todos os preços com o desconto
-            write('Todos os preços foram atualizados com desconto de: '), write(Desconto), write('%'), nl
+    atom_number(CupomAtom, CupomInput),
+    (remover_cupom(CupomAtom, TabelaHashCupom, NovaTabelaHash, Desconto) ->
+        write('Desconto aplicado: '), write(Desconto), write('%'), nl,
+        write('Digite o ID do item para atualizar o preço: '), read(ItemID),
+        (ler_item(ItemID, item(Nome, Estoque, Preco)) ->
+            NovoPreco is Preco * (1 - Desconto / 100),
+            atualizar_item(ItemID, item(Nome, Estoque, NovoPreco)),
+            write('Preço do item atualizado para: '), write(NovoPreco), nl,
+            registrar_acao('Desconto aplicado ao item')  % Log action
         ;
-            write('Código do cupom inválido.'), nl
+            write('Item não encontrado.'), nl
         )
     ;
-        write('Entrada inválida para o código do cupom.'), nl
+        write('Código do cupom inválido.'), nl
     ),
     menu(NovaTabelaHash, Funcionarios, FuncaoAtual).
 
+executar(11, TabelaHashCupom, Funcionarios, FuncaoAtual) :- 
+    gerar_relatorio,  % Generate report
+    registrar_acao('Relatório gerado'),  % Log action
+    menu(TabelaHashCupom, Funcionarios, FuncaoAtual).
 
-executar(11, _, _, _) :- 
+executar(12, TabelaHashCupom, Funcionarios, FuncaoAtual) :- 
+    limpar_relatorio,  % Clear report
+    registrar_acao('Relatório limpo'),  % Log action
+    menu(TabelaHashCupom, Funcionarios, FuncaoAtual).
+
+executar(13, _, _, _) :- 
     write('Obrigado por usar o sistema. Até logo!'), nl,
+    registrar_acao('Sistema encerrado'),  % Log action
     halt.
-
-
 
 executar(_, TabelaHashCupom, Funcionarios, FuncaoAtual) :- 
     write('Opção inválida! Tente novamente.'), nl,
